4227fc47e66723e02b78b2fa7e5fad0e76760618
Shaggae <t.umathorn@gmail.com>
2025-01-13 10:50:27 +0800

    become a coach page

---
+++ badpjProject/BecomeACoach.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1loggedin.Master" AutoEventWireup="true" CodeBehind="BecomeACoach.aspx.cs" Inherits="badpjProject.BecomeACoach" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h1>Apply to Become a Coach</h1><br />
+        <p>
+            NAME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tb_Name" runat="server" Width="500px"></asp:TextBox>
+        </p>
+        <p>
+            EMAIL&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; <asp:TextBox ID="tb_Email" runat="server" Width="500px"></asp:TextBox>
+        </p>
+        <p>
+            HP&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; <asp:TextBox ID="tb_Hp" runat="server" Width="500px"></asp:TextBox>
+        </p>
+        <p>
+            ABOUT YOU&nbsp;&nbsp;
+            <asp:TextBox ID="tb_AboutYou" runat="server" Height="103px" Width="500px"></asp:TextBox>
+            &nbsp;&nbsp;
+        </p>
+        <asp:Panel ID="Panel1" runat="server" GroupingText="QUALIFICATION">
+            <asp:DropDownList ID="ddl_Qualification" runat="server">
+                <asp:ListItem Value="Placeholder Rank"></asp:ListItem>
+                <asp:ListItem Value="Certified-PT"></asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <h5>Video showcasing your expertise&nbsp;&nbsp;</h5>
+            <asp:FileUpload ID="fu_Coach" runat="server" placeholder="Showcase your expertise" Width="500px" />
+        </asp:Panel>
+        <br />
+        <asp:Button ID="btn_Submit" runat="server" Text="Submit" OnClick="btn_Submit_Click" />
+    </div>
+</asp:Content>
---
+++ badpjProject/BecomeACoach.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace badpjProject
+{
+    public partial class BecomeACoach : System.Web.UI.Page
+    {
+        public string GenerateCoachId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tb_Name.Text) ||
+                string.IsNullOrWhiteSpace(tb_Email.Text) ||
+                string.IsNullOrWhiteSpace(tb_Hp.Text) ||
+                string.IsNullOrWhiteSpace(tb_AboutYou.Text) ||
+                string.IsNullOrWhiteSpace(ddl_Qualification.SelectedValue) ||
+                !fu_Coach.HasFile)
+            {
+                Response.Write("<script>alert('All fields are required');</script>");
+                return;
+            }
+
+            int result = 0;
+            string image = "";
+
+
+            if (fu_Coach.HasFile)
+            {
+                image = fu_Coach.FileName;
+            }
+
+            string generatedId = GenerateCoachId();
+            string status = "Pending";
+
+            Coaches coach = new Coaches(generatedId, tb_Name.Text, tb_Email.Text, 
+                                        int.Parse(tb_Hp.Text), tb_AboutYou.Text, ddl_Qualification.Text, image, status);
+            result = coach.CoachesInsert();
+
+            if (result > 0)
+            {
+                string saveimg = Server.MapPath("~/Uploads/") + image;
+                fu_Coach.SaveAs(saveimg);
+                Response.Write("<script>alert('Submission successful');</script>");
+            }
+            else { Response.Write("<script>alert('Submission NOT successful');</script>"); }
+
+
+            Response.Redirect("Coaches.aspx");
+        }
+    }
+}
