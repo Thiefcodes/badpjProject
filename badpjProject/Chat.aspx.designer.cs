4b53bbb6cc999844e03cda74a23172812de3bc73
shirako1022 <tayzhikai1022@gmail.com>
2025-02-17 01:24:37 +0800

    chat offer

--- badpjProject/Chat.aspx.designer.cs
+++ badpjProject/Chat.aspx.designer.cs
@@ -58,5 +58,32 @@ namespace badpjProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSend;
+
+        /// <summary>
+        /// pnlOfferSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlOfferSection;
+
+        /// <summary>
+        /// txtOfferPriceField control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtOfferPriceField;
+
+        /// <summary>
+        /// btnSendOffer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSendOffer;
     }
 }
